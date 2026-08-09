d1238cea1172b8ca3958193086f5d4ca1aab1d6a
Nisha Sharma <nishaasharma0707@gmail.com>
2026-08-09 22:24:20 +0530

    Time: 0 ms (100%), Space: 41.8 MB (94.57%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1){
+            return 1;
+        }
+        int one=1;
+        int two=2;
+        for(int i=3; i<=n;i++){
+            int total=one+two;
+            one=two;
+            two=total;
+        }
+        return two;
+    }
+}
